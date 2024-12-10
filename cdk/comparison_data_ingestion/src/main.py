a77275de803cf95e6dcfb8ff724b72ec7f4a2cbf
Harshinee Sriram <hsriram@cs.ubc.ca>
2024-12-10 14:45:08 -0800

    Delete document after its data has been ingested.
    Lines 113-127 have been edited.

--- cdk/comparison_data_ingestion/src/main.py
+++ cdk/comparison_data_ingestion/src/main.py
@@ -42,113 +42,6 @@ def get_secret():
     secret = json.loads(response)
     return secret
 
-# def connect_to_db():
-#     try:
-#         db_secret = get_secret()
-#         connection_params = {
-#             'dbname': db_secret["dbname"],
-#             'user': db_secret["username"],
-#             'password': db_secret["password"],
-#             'host': db_secret["host"],
-#             'port': db_secret["port"]
-#         }
-#         connection_string = " ".join([f"{key}={value}" for key, value in connection_params.items()])
-#         connection = psycopg2.connect(connection_string)
-#         logger.info("Connected to the database!")
-#         return connection
-#     except Exception as e:
-#         logger.error(f"Failed to connect to database: {e}")
-#         if connection:
-#             connection.rollback()
-#             connection.close()
-#         return None
-
-# def parse_s3_file_path(document_key):
-#     # Assuming the file path is of the format: {category_id}/{document_name}.{document_type}
-#     try:
-#         category_id, documentname_with_ext = document_key.split('/')
-#         document_name, document_type = documentname_with_ext.split('.')
-#         return category_id, document_name, document_type
-#     except Exception as e:
-#         logger.error(f"Error parsing S3 document path: {e}")
-#         return {
-#                     "statusCode": 400,
-#                     "body": json.dumps("Error parsing S3 document path.")
-#                 }
-
-# def insert_file_into_db(category_id, document_name, document_type, document_s3_file_path):
-#     connection = connect_to_db()
-#     if connection is None:
-#         logger.error("No database connection available.")
-#         return {
-#             "statusCode": 500,
-#             "body": json.dumps("Database connection failed.")
-#         }
-    
-    # try:
-    #     cur = connection.cursor()
-
-    #     # Check if a record already exists
-    #     select_query = """
-    #     SELECT * FROM "documents"
-    #     WHERE category_id = %s
-    #     AND document_name = %s
-    #     AND document_type = %s;
-    #     """
-    #     cur.execute(select_query, (category_id, document_name, document_type))
-
-    #     existing_document = cur.fetchone()
-
-    #     if existing_document:
-    #         # Update the existing record
-    #         update_query = """
-    #             UPDATE "documents"
-    #             SET document_s3_file_path = %s,
-    #             time_created = %s
-    #             WHERE category_id = %s
-    #             AND document_name = %s
-    #             AND document_type = %s;
-    #         """
-    #         timestamp = datetime.now(timezone.utc)
-    #         cur.execute(update_query, (
-    #             document_s3_file_path,  # filepath
-    #             timestamp,  # time_uploaded
-    #             category_id,  # module_id
-    #             document_name,  # filename
-    #             document_type  # filetype
-    #         ))
-    #         logger.info(f"Successfully updated file {document_name}.{document_type} in database for module {category_id}.")
-    #     else:
-    #         # Insert a new record
-    #         insert_query = """
-    #             INSERT INTO "documents" 
-    #             (category_id, document_s3_file_path, document_name, document_type, metadata, time_created)
-    #             VALUES (%s, %s, %s, %s, %s, %s);
-    #         """
-    #         timestamp = datetime.now(timezone.utc)
-    #         cur.execute(insert_query, (
-    #             category_id,  # module_id
-    #             document_s3_file_path,
-    #             document_name,  # filename
-    #             document_type, # filetype
-    #             "",
-    #             timestamp
-
-    #     ))
-    #     logger.info(f"Successfully inserted document {document_name}.{document_type} into database for module {category_id}.")
-
-    #     connection.commit()
-    #     cur.close()
-    #     connection.close()
-    # except Exception as e:
-    #     if cur:
-    #         cur.close()
-    #     if connection:
-    #         connection.rollback()
-    #         connection.close()
-    #     logger.error(f"Error inserting document {document_name}.{document_type} into database: {e}")
-    #     raise
-
 def update_vectorstore_from_s3(bucket, session_id):
     # bucket = "DSA-data-ingestion-bucket"
     bedrock_runtime = boto3.client(
@@ -191,18 +84,8 @@ def handler(event, context):
             "statusCode": 400,
             "body": json.dumps("No valid S3 event found.")
         }
-
-    # for record in records:
-    #     event_name = record['eventName']
-    #     bucket_name = record['s3']['bucket']['name']
+        
     bucket_name = DSA_COMPARISON_BUCKET
-
-
-        # # Only process files from the DSA_DATA_INGESTION_BUCKET
-        # if bucket_name != DSA_COMPARISON_BUCKET:
-        #     print(f"Ignoring event from non-target bucket: {bucket_name}")
-        #     continue  # Ignore this event and move to the next one
-        # document_key = record['s3']['object']['key']
 
     for record in records:
         # Extract the message body from the SQS event
@@ -217,38 +100,6 @@ def handler(event, context):
 
         # Assuming the file path is of the format: {session_id}/{filename}
         document_key = f"{session_id}/{filename}.{file_type}"
-
-    
-
-
-        # # Parse the file path
-        # category_id, document_name, document_type = parse_s3_file_path(document_key)
-        # if not category_id or not document_name or not document_type:
-        #     return {
-        #             "statusCode": 400,
-        #             "body": json.dumps("Error parsing S3 file path.")
-        #     }
-
-        # if event_name.startswith('ObjectCreated:'):
-        #     # Insert the file into the PostgreSQL database
-        #     try:
-        #         insert_file_into_db(
-        #             category_id=category_id,
-        #             document_name=document_name,
-        #             document_type=document_type,
-        #             document_s3_file_path=document_key
-        #         )
-        #         logger.info(f"File {document_name}.{document_type} inserted successfully.")
-        #     except Exception as e:
-        #         logger.error(f"Error inserting file {document_name}.{document_type} into database: {e}")
-        #         return {
-        #             "statusCode": 500,
-        #             "body": json.dumps(f"Error inserting file {document_name}.{document_type}: {e}")
-        #         }
-            
-        # else:
-        #     logger.info(f"File {document_name}.{document_type} is being deleted. Deleting files from database does not occur here.")
-        # Update embeddings for course after the file is successfully inserted into the database
         
         try:
                 update_vectorstore_from_s3(bucket_name, session_id)
@@ -259,11 +110,18 @@ def handler(event, context):
                     "statusCode": 500,
                     "body": json.dumps(f"Document inserted, but error updating vectorstore: {e}")
                 }
+        # If update_vectorstore_from_s3() was executed successfully, the following code snippet removes this document from the s3 bucket
+        s3_client = boto3.client('s3')
+        try:
+            s3_client.delete_object(Bucket=bucket_name, Key=document_key)
+            logger.info(f"Successfully deleted {document_key} from {bucket_name} after vectorstore update.")
+        except Exception as e:
+            logger.error(f"Error deleting {document_key} from {bucket_name}: {e}")
 
         return {
                 "statusCode": 200,
                 "body": json.dumps({
-                    "message": "New file inserted into database.",
+                    "message": "New file inserted into database. Vectorstore updated and document deleted from S3 successfully.",
                     "location": f"s3://{bucket_name}/{document_key}"
                 })
             }
