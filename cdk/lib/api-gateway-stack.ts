e45e5dd06b7db7b4a9dc0c941c1323847c0d222e
Kanish Khanna <90278512+kanish10@users.noreply.github.com>
2025-01-20 15:10:03 -0800

    added permission for dynamodb update

--- cdk/lib/api-gateway-stack.ts
+++ cdk/lib/api-gateway-stack.ts
@@ -635,6 +635,7 @@ export class ApiGatewayStack extends cdk.Stack {
           "dynamodb:DescribeTable",
           "dynamodb:PutItem",
           "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
         ],
         resources: [`arn:aws:dynamodb:${this.region}:${this.account}:table/*`],
       })
