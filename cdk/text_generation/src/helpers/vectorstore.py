006c79a3f87a4b27c38cb5701fb2676c7c494dc1
Harshinee Sriram <hsriram@cs.ubc.ca>
2025-02-20 14:21:45 -0800

    Update vectorstore.py

--- cdk/text_generation/src/helpers/vectorstore.py
+++ cdk/text_generation/src/helpers/vectorstore.py
@@ -15,12 +15,12 @@ def get_vectorstore_retriever(
     Retrieve the vectorstore and return the history-aware retriever object.
 
     Args:
-    llm: The language model instance used to generate the response.
-    vectorstore_config_dict (Dict[str, str]): The configuration dictionary for the vectorstore, including parameters like collection name, database name, user, password, host, and port.
-    embeddings (BedrockEmbeddings): The embeddings instance used to process the documents.
+        llm: The language model instance used to generate the response.
+        vectorstore_config_dict (Dict[str, str]): The configuration dictionary for the vectorstore, including parameters like collection name, database name, user, password, host, and port.
+        embeddings (BedrockEmbeddings): The embeddings instance used to process the documents.
 
     Returns:
-    VectorStoreRetriever: A history-aware retriever instance.
+        VectorStoreRetriever: A history-aware retriever instance.
     """
     vectorstore, _ = get_vectorstore(
         collection_name=vectorstore_config_dict['collection_name'],
@@ -33,7 +33,6 @@ def get_vectorstore_retriever(
     )
 
     retriever = vectorstore.as_retriever()
-    print(f"exiting get_vectorstore")
     # Contextualize question and create history-aware retriever
     contextualize_q_system_prompt = (
         "Given a chat history and the latest user question "
@@ -43,7 +42,6 @@ def get_vectorstore_retriever(
         "just reformulate it if needed and otherwise return it as is."
     )
 
-    print(f"before from_messages")
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", contextualize_q_system_prompt),
@@ -51,10 +49,9 @@ def get_vectorstore_retriever(
             ("human", "{input}"),
         ]
     )
-    print(f"after from_messages")
+
     history_aware_retriever = create_history_aware_retriever(
         llm, retriever, contextualize_q_prompt
     )
-    print(f"exiting get_vectorstore_retriever")
 
     return history_aware_retriever
